947d82d15d0ccbce6b9788c4ca4791ac832dcfd5
Krasimir <gdanailov@qualibooth.com>
2024-03-14 17:46:30 +0100

    sample

--- main.js
+++ main.js
@@ -1,24 +1,61 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+//3D objects - torus
+const geometry = new THREE.TorusKnotGeometry( 10/3, 1, 300, 20); 
+const material = new THREE.MeshNormalMaterial();
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+torus.position.z = 10;
+torus.position.setX(-5);
+//Human
+
+
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+//Update Camera Movement
+function moveCamera(){
+  const t = document.body.getBoundingClientRect().bottom;
+  
+  torus.rotation.x += 0.05;
+  torus.rotation.y += 0.075;
+  torus.rotation.z += 0.05;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+document.body.onscroll = moveCamera;
+moveCamera();
+
+//Animation
+function animate(){
+  requestAnimationFrame(animate);
+  
+  //Moving
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
