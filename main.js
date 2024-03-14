fb0115db54b2fb33c9f85b8317246133d270f274
Krasimir <gdanailov@qualibooth.com>
2024-03-14 21:46:48 +0100

    sample2

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 const scene = new THREE.Scene();
@@ -17,12 +16,13 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 //3D objects - torus
-const geometry = new THREE.TorusKnotGeometry( 10/3, 1, 300, 20); 
+const geometry = new THREE.TorusKnotGeometry( 10/1.85, 1.25, 300, 20); 
 const material = new THREE.MeshNormalMaterial();
 const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 torus.position.z = 10;
 torus.position.setX(-5);
+torus.position.setY(-5);
 //Human
 
 
@@ -32,15 +32,15 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 //Update Camera Movement
 function moveCamera(){
-  const t = document.body.getBoundingClientRect().bottom;
+  const t = document.body.getBoundingClientRect().top;
   
   torus.rotation.x += 0.05;
   torus.rotation.y += 0.075;
   torus.rotation.z += 0.05;
 
   camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
+  camera.position.x = 5;
+  camera.position.y = 10 - window.scrollY/500;
 }
 document.body.onscroll = moveCamera;
 moveCamera();
