af57ae2ef8987ecba32582a482a3d713b82f88e6
Krasimir <gdanailov@qualibooth.com>
2024-03-15 11:44:42 +0100

    astronat

--- main.js
+++ main.js
@@ -1,31 +1,54 @@
 import './style.css'
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-
-const scene = new THREE.Scene();
+const scene1 = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
-
+//First renderer
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+renderer.render(scene1, camera);
+//Second
+ 
 
-renderer.render(scene, camera);
+
+
+//Light
+const light = new THREE.SpotLight(0xffffff, 100);
+light.position.set(5,5,5);
+scene1.add(light);
+
 
 //3D objects - torus
 const geometry = new THREE.TorusKnotGeometry( 10/1.85, 1.25, 300, 20); 
 const material = new THREE.MeshNormalMaterial();
 const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+scene1.add(torus);
 torus.position.z = 10;
 torus.position.setX(-5);
 torus.position.setY(-5);
-//Human
+//Astronat model
+const loader = new GLTFLoader();
+loader.load(
+  'assets/space_suit/scene.gltf',
+  function(gltf){
+    const box = gltf.scene;
+    box.traverse((child) => {
+      if (child.isMesh) child.material = material;
+    });
 
 
+    box.position.z = 5;
+    box.scale.set(4,4,4);
+    scene1.add(box);
+    renderer.render(scene1, camera);
+  } 
+);
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -55,7 +78,7 @@ function animate(){
   torus.rotation.z += 0.01;
 
   controls.update();
-  renderer.render(scene, camera);
+  renderer.render(scene1, camera);
 }
 
 animate();
